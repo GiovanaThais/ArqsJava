af52147319dac692266cb75afe99f814c47286a5
GiovanaThais <41754881+GiovanaThais@users.noreply.github.com>
2020-08-30 18:10:53 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,11 @@
+package sistema_compras;
+
+public class Produto {
+    
+    String identificador;
+    String nomeProduto;
+    String marca;
+    double valor;
+    int disponibilidade;
+    
+}
