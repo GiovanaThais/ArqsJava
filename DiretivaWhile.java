51a0fb17dbf99f9b3b654d7facafb483cfb4455a
GiovanaThais <41754881+GiovanaThais@users.noreply.github.com>
2020-08-29 21:25:40 -0300

    Add files via upload

---
+++ DiretivaWhile.java
@@ -0,0 +1,16 @@
+public class DiretivaWhile {
+    /*public static void main(String[] args) {
+        int j=10;
+        while(j>=0){
+        System.out.println(j);
+        j--;
+        }
+    }*/
+    public static void main(String[] args) {
+        int i=0;
+        do{
+        System.out.println(i);
+        ++i;
+        }while(i<=10);
+    }
+}
