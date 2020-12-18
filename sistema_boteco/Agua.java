8c6add58cb1bda7b365ce497100c4e02193eafc7
GiovanaThais <41754881+GiovanaThais@users.noreply.github.com>
2020-12-18 09:54:49 -0300

    Add files via upload

---
+++ sistema_boteco/Agua.java
@@ -0,0 +1,20 @@
+package sistema_boteco;
+public class Agua {
+    public static void main(String[] args) {
+        private String marca;
+        private double valor;
+
+        public String getMarca(){
+            return marca;
+        }
+        public void setMarca(String marca) {
+            this.marca = marca;
+        }
+        public double getValor() {
+            return valor;
+        }
+        public void setValor(double valor) {
+            this.valor = valor;
+        }
+    }
+}
