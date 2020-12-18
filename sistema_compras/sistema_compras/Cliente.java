0b41851720957a7a10ea585ba3d76fb8961df9d0
GiovanaThais <41754881+GiovanaThais@users.noreply.github.com>
2020-12-18 09:54:01 -0300

    Add files via upload

---
+++ sistema_compras/sistema_compras/Cliente.java
@@ -0,0 +1,12 @@
+package sistema_compras;
+public class Cliente{
+
+    public int cpf;
+    public String nome;
+    public String endereco;
+    public int telefone;
+    public String email;
+    public double limiteCompras;
+
+
+}
---
+++ sistema_compras/sistema_compras/Produto.java
@@ -0,0 +1,11 @@
+package sistema_compras;
+
+public class Produto {
+
+    public String identificador;
+    public String nomeProduto;
+    public String marca;
+    public double valor;
+    public int disponibilidade;
+    
+}
